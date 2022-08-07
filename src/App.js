8e2c8c03a697a2cce68c6fa8fe0878eb4c4f3d86
Hasan-aga <careerforma@gmail.com>
2022-08-07 18:31:29 +0300

    add sass

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
     <div className="categories-container">
       {categories.map(({ id, title }) => (
         <div key={id} className="category">
+          <div className="category-background" />
           {/* <img/> */}
           <h2>{title}</h2>
           <p>Shop Now</p>
