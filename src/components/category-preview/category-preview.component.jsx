6ad99747cd9ba18addb71e97e0f165a4fc4251dd
Hasan-aga <careerforma@gmail.com>
2022-08-11 23:39:08 +0300

    finish categories sub route

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,13 +1,20 @@
 import "./category-preview.style.scss";
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 import ProductCard from "../product-card/product-card.component";
 
 const CategoryPreview = ({ title, products, previewIndex }) => {
   return (
     <Fragment>
       <div className="category-title">
-        {" "}
-        <span>{title}</span>
+        {previewIndex ? (
+          <Link to={title}>
+            {" "}
+            <span>{title}</span>
+          </Link>
+        ) : (
+          <span>{title}</span>
+        )}
       </div>
       <div className="shop-products">
         {products.slice(0, previewIndex).map((product) => (
