9d9b868f72239c1a87926573f60638e4abeabad4
Hasan-aga <careerforma@gmail.com>
2022-08-24 10:05:35 +0300

    hide dropdown instead of not rendering

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,6 +6,8 @@ import { DropDown, EmptyCart } from "./cart-dropdown.style.jsx";
 import { selectCartProducts } from "../../store/cart/cart.selector";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleDropdown } from "../../store/cart/cart-action";
+import { useCloseDropdownOnOutsideClick } from "../../custom-hooks/useCloseDropdownOnOutsideClick";
+import "./hide.style.scss";
 
 const CartDropdown = (props) => {
   const dispatch = useDispatch();
@@ -14,35 +16,36 @@ const CartDropdown = (props) => {
     dispatch(toggleDropdown(true));
   };
 
-  const cartProducts = useSelector(selectCartProducts);
-  return props.active ? (
-    <DropDown>
-      {cartProducts.length === 0 ? (
-        <EmptyCart>
-          <span>Your cart is empty</span>
-        </EmptyCart>
-      ) : (
-        <Fragment>
-          <div className="cart-items">
-            {cartProducts.map((product) => (
-              <DropdownItem key={product.id} item={product} />
-            ))}
-          </div>
+  const ref = useCloseDropdownOnOutsideClick(closeDropdown, props.active);
 
-          <Link to="/checkout">
-            <div>
-              <CustomButton
-                onClick={closeDropdown}
-                className="cart-button"
-                label="go to checkout"
-              />
+  const cartProducts = useSelector(selectCartProducts);
+  return (
+    <div ref={ref} className={props.active ? "" : "hide"}>
+      <DropDown className="dropdown-menu">
+        {cartProducts.length === 0 ? (
+          <EmptyCart>
+            <span>Your cart is empty</span>
+          </EmptyCart>
+        ) : (
+          <Fragment>
+            <div className="cart-items">
+              {cartProducts.map((product) => (
+                <DropdownItem key={product.id} item={product} />
+              ))}
             </div>
-          </Link>
-        </Fragment>
-      )}
-    </DropDown>
-  ) : (
-    <Fragment />
+            <Link to="/checkout">
+              <div>
+                <CustomButton
+                  onClick={closeDropdown}
+                  className="cart-button"
+                  label="go to checkout"
+                />
+              </div>
+            </Link>
+          </Fragment>
+        )}
+      </DropDown>
+    </div>
   );
 };
 
