d97d638b90bdd90a3dea38aa6884049ea1026664
Hasan-aga <careerforma@gmail.com>
2022-08-30 18:57:01 +0300

    type custom logger

---
+++ src/utils/store/customLogger.ts
@@ -0,0 +1,17 @@
+import { Middleware } from "redux";
+import { RootState } from "../../store/store";
+
+export const loggerMiddleware: Middleware<{}, RootState> =
+  (store) => (next) => (action) => {
+    if (!action.type) return next(action);
+
+    console.log("type: ", action.type);
+    console.log("payload: ", action.payload);
+    console.log("current state: ", store.getState());
+
+    //   pass the action to reducers and update the store
+    next(action);
+
+    // now we can log the next state
+    console.log("next state: ", store.getState());
+  };
