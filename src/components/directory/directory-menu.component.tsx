1335c38998aa6d29fe1dc28ad835a2434ccfddc1
Hasan-aga <careerforma@gmail.com>
2022-09-29 11:05:51 +0300

    convert directory to tsx

--- src/components/directory/directory-menu.component.tsx
+++ src/components/directory/directory-menu.component.tsx
@@ -30,7 +30,7 @@ const categories = [
       "https://images.unsplash.com/photo-1620921597945-908e7947c41f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1634&q=80",
   },
 ];
-const CategoryMenu = ({ category }) => {
+const CategoryMenu = () => {
   return categories.map((cat) => (
     <Link to={`shop/${cat.title}`} key={cat.id + cat.title}>
       <Category
